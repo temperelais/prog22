b39ebac1bc92bebee4c13782c97d22a4b2f01647
temperelais <149486977+temperelais@users.noreply.github.com>
2024-09-10 11:23:02 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,11 @@
-print(1+1)
+class Human:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+person = Human("Janis",3,"male")
+
+print(person.name)
+print(person.gender)
+print(person.age)
