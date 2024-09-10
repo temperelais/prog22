13505a9578934707cfa41ffb19e2a5df6dd19942
temperelais <149486977+temperelais@users.noreply.github.com>
2024-09-10 12:26:23 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -7,7 +7,17 @@ class Human:
     def birthday(self):
         self.age += 1
 
-        
+    def namechange(self,newname):
+        self.name=newname
+    def intro(self):
+        print("Sveiki, mans dzimums ir", self.gender)
+        if self.gender == "male":
+            print("esmu",self.age,"gadus vecs")
+        else:
+            print("esmu",self.age,"gadus veca")
+        print("Mani sauc",self.name)
+    def genderchange(self,newg):
+        self.gender=newg
 
 person = Human("sdjsdjiw",3,"male")
 
@@ -19,11 +29,9 @@ person.birthday()
 
 print(person.age)
 
-print("Sveiki, mans dzimums ir", person.gender)
-if person.gender == "male":
-    print("esmu",person.age,"gadus vecs")
-else:
-    print("esmu",person.age,"gadus veca")
-print("Mani sauc",person.name)
 
+
+person.namechange("yeajkls")
+
+person
 
